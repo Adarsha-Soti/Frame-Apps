25e87e089b6a1ad770a34df81219309485cf54e5
Adarsha Soti <aadarshasoti01@gmail.com>
2025-12-15 07:03:59 +1100

    functions updated but not complete

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-let buttons= document.getElementsByClassName("btn");
 let screenSection=document.querySelector(".screenSection");
+let buttons=document.getElementsByClassName("buttonSection");
 
+const operatorBtn=["x","%","+","-","/"];
+let lastClickedbtn=null;
 
-const displayvalue=(value)=>{
-    screenSection.innerText+=value;  
+const display =(values)=>{
+screenSection.innerText+=values;
 }
 
-for (let btn of buttons){
-    btn.addEventListener("click",(e)=>{
-    let btnValues =e.target.innerText;
-    let evaluate;
-    if (btnValues=="="){
-    try{
-        
-        evaluate=eval(screenSection.innerText);
-    }
-    catch{
-        evaluate="";
-    }
-    console.log(evaluate);
-    screenSection.innerText="";
-    displayvalue(evaluate);
-    }else if (btnValues=="C"){
-        screenSection.innerText="";
-    } else{
-    console.log(btnValues);
-    displayvalue(btnValues);
+function clickedEvent(){
+    for (let btn of buttons){
+       btn.addEventListener("click",(e)=>{
+           let clickedBtn=e.target.innerText;
+        // just to get value outside this fucking function
+           lastClickedbtn=clickedBtn;
+           //once button is clicked it would check conditions
+            checkCondition(clickedBtn);
         }
+    )
+    }
+    
+};
+// to check conditions 
+const checkCondition=(btnValue)=>{
+    let lastChar=(screenSection.innerText).slice(-1);
+    let currentText= screenSection.innerText;
+    let newText=currentText.slice(0,-1);
 
-        }) 
+    // if(operatorBtn.includes(lastChar)&& operatorBtn.includes(lastClickedbtn)){
+    //  return;
+    // }else{
+        if (btnValue=="x"){
+        display("*")
+        }else if(btnValue=="C") {
+            screenSection.innerText=null;
+        }else if(btnValue=="DEL") {
+        screenSection.innerText=newText;  
+        }
+        else if (btnValue=="="){
+            if (lastClickedbtn!=operatorBtn){
+                let evaluate= eval(screenSection.innerText).toString();
+                screenSection.innerText=null;
+                display(evaluate);
+            }
+            
+        }
+        else{
+            display(lastClickedbtn);
+        };
+    // }
+        
 }
-
+clickedEvent();
